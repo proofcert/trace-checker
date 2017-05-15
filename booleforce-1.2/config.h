de01b74c8007f8148c98607aa5e0000edf4bf0f8
Xaviera <a92347@aup.edu>
2017-05-15 11:53:59 +0000

    bash scripts, the main prolog code+assert version in progress, SAT files and booleforce

---
+++ booleforce-1.2/config.h
@@ -0,0 +1,12 @@
+#ifndef BOOLEFORCE_bfconfig_h_INCLUDED
+#define BOOLEFORCE_bfconfig_h_INCLUDED
+#define BOOLEFORCE_VERSION "1.2"
+#define BOOLEFORCE_OS "Linux xavxav-prolog-4435988 4.9.17-c9 #1 SMP Thu Mar 23 01:38:54 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux"
+#define BOOLEFORCE_CC "gcc"
+#define BOOLEFORCE_CCVERSION "4.8"
+#define BOOLEFORCE_CFLAGS "-Wall -W -Werror -O2"
+#define NDEBUG
+#define BOOLEFORCE_TRACE
+typedef long int64;
+#define INT64FMT "ld"
+#endif
